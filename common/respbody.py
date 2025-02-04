c432eeb8801a8bf7a0479692dba322377eb162f4
ech09527 <ech09527@outlook.com>
2025-02-04 23:03:31 +0800

    添加配置，优化相应

--- common/respbody.py
+++ common/respbody.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
-from typing import List,TypeVar,Generic
+from typing import List,TypeVar,Generic, Union
 from loguru import logger
 from functools import wraps
-
+from traceback import format_exc
 RESP_BODY_DATA_TYPE = TypeVar('T')
 class RespBody(BaseModel, Generic[RESP_BODY_DATA_TYPE]):
     status: int = 0
-    msg: str = None
-    data: RESP_BODY_DATA_TYPE = None
+    msg: Union[str, None]
+    data: Union[RESP_BODY_DATA_TYPE, None]
     
     
 
@@ -16,9 +16,10 @@ def common_resp(func):
     async def wrapper(*args, **kwargs):
         try:
             res = await func(*args, **kwargs)
+            res = RespBody(data=res,msg="success",status=0)
         except Exception as e:
-            logger.error(e)            
-            return RespBody(status=1,msg=str(e))
-        return RespBody(data=res)
+            logger.error(format_exc())
+            res = RespBody(status=1,msg=str(e),data=None)
+        return res
     wrapper.__annotations__['return'] = RespBody[func.__annotations__['return']]
     return wrapper
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from router.strategy_exec_info import fastapi_app
 from tortoise.contrib.fastapi import register_tortoise
+from config import settings
 
 register_tortoise(
     fastapi_app,
-    db_url="mysql://lh3_api:ECF7773F15@rm-j6c4gtw0039u0ia32ko.mysql.rds.aliyuncs.com:3306/lh3_test",
+    db_url=settings.db.url,
     modules={"models": ["models"]},
     generate_schemas=False,
     add_exception_handlers=True
